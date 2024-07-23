f6d134f5c8f5e69b8e15c4b16da6d616ec356a61
Chandan Kumar Dash <134797174+ckdash-git@users.noreply.github.com>
2024-07-23 14:05:34 +0530

    Add files via upload

---
+++ Mover/Mover/MoverApp.swift
@@ -0,0 +1,17 @@
+//
+//  MoverApp.swift
+//  Mover
+//
+//  Created by Chandan Kumar Dash on 23/07/24.
+//
+
+import SwiftUI
+
+@main
+struct MoverApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
